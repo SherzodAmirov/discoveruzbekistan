e21036fef4cc27fc0057ec9c8d2baf7009205ecf
SherzodAmirov <amirovrich@gmail.com>
2026-03-15 03:32:09 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,57 @@
-var map = L.map('map').setView([41.2995, 69.2401], 6);
+// Data
+const places = {
+"registan":{
+name:"Registan",
+desc:"Samarkanddagi mashhur tarixiy maydon",
+distance:"Masofa: 300 km",
+cost:"Xarajat: ~250000 so'm",
+coords:[39.6542,66.9750]
+},
+"charvak":{
+name:"Charvak",
+desc:"Toshkent yaqinidagi chiroyli suv va tog' joyi",
+distance:"Masofa: 80 km",
+cost:"Xarajat: ~150000 so'm",
+coords:[41.6170,70.0800]
+},
+"bukhara":{
+name:"Bukhara",
+desc:"Qadimiy shahar va tarixiy obidalar",
+distance:"Masofa: 600 km",
+cost:"Xarajat: ~400000 so'm",
+coords:[39.7747,64.4286]
+}
+};
 
+// Show modal
+function showDetails(placeId){
+let place = places[placeId];
+document.getElementById("placeName").innerText = place.name;
+document.getElementById("placeDesc").innerText = place.desc;
+document.getElementById("placeDistance").innerText = place.distance;
+document.getElementById("placeCost").innerText = place.cost;
+document.getElementById("modal").style.display = "flex";
+document.getElementById("map").style.display = "none";
+}
+
+// Close modal
+function closeModal(){
+document.getElementById("modal").style.display = "none";
+}
+
+// Show map in modal
+function showMap(){
+let mapDiv = document.getElementById("map");
+mapDiv.style.display = "block";
+let map = L.map('map').setView([41.2995, 69.2401], 6);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
 maxZoom: 18
 }).addTo(map);
 
-// Registan
-L.marker([39.6542,66.9750])
+// Add markers
+for(let key in places){
+L.marker(places[key].coords)
 .addTo(map)
-.bindPopup("<b>Registan</b><br>Distance: 300 km<br>Cost: ~250000 so'm");
-
-// Charvak
-L.marker([41.6170,70.0800])
-.addTo(map)
-.bindPopup("<b>Charvak</b><br>Distance: 80 km<br>Cost: ~150000 so'm");
-
-// Bukhara
-L.marker([39.7747,64.4286])
-.addTo(map)
-.bindPopup("<b>Bukhara</b><br>Distance: 600 km<br>Cost: ~400000 so'm");
+.bindPopup("<b>"+places[key].name+"</b><br>"+places[key].distance+"<br>"+places[key].cost);
+}
+}
